9e592b0f5afd4d0d7601d4bd6d206e3fff86b768
pavalex <Alex.P79@mail.ru>
2018-08-09 19:39:55 +0800

    geo maps

--- src/js/main.js
+++ src/js/main.js
@@ -1,78 +1,84 @@
-$ (document).ready (function () {
+$(document).ready(function () {
   /* popup */
-  $ ('.header-contacts__button').on ('click', function () {
-    $ ('.overlay').fadeIn (500);
+  $('.header-contacts__button').on('click', function () {
+    $('.overlay').fadeIn(500);
   });
 
-  $ ('.popup-close').on ('click', function () {
-    $ ('.overlay').fadeOut (500);
+  $('.contacts-block__btn').on('click', function () {
+    $('.overlay').fadeIn(500);
   });
 
-  $ ('.overlay').click (function () {
-    $ (document).on ('click', function (event) {
-      var select = $ ('.popup');
-      if ($ (event.target).closest (select).length) return;
-      $ ('.overlay').fadeOut (500);
-      $ (document).unbind ('click');
-      event.stopPropagation ();
+  $('.btn-send').on('click', function () {
+    $('.overlay').fadeIn(500);
+  });
+
+  $('.popup-close').on('click', function () {
+    $('.overlay').fadeOut(500);
+  });
+
+  $('.overlay').click(function () {
+    $(document).on('click', function (event) {
+      var select = $('.popup');
+      if ($(event.target).closest(select).length) return;
+      $('.overlay').fadeOut(500);
+      $(document).unbind('click');
+      event.stopPropagation();
     });
   });
 
   /* add class*/
-  function classFunction () {
-    if ($ ('body').width () < 991.98) {
-      $ ('.menu-item').addClass ('menu-item-mobile');
+  function classFunction() {
+    if ($('body').width() < 991.98) {
+      $('.menu-item').addClass('menu-item-mobile');
     } else {
-      $ ('.menu-item').removeClass ('menu-item-mobile');
+      $('.menu-item').removeClass('menu-item-mobile');
     }
   }
 
-  classFunction ();
-  $ (window).resize (classFunction);
+  classFunction();
+  $(window).resize(classFunction);
 
   /* menu */
-  $ ('.header-link__burger').on ('click', function () {
-    $ ('.menu').fadeIn (200);
+  $('.header-link__burger').on('click', function () {
+    $('.menu').fadeIn(200);
   });
 
-  $ ('.menu-close').on ('click', function () {
-    $ ('.menu').fadeOut (200);
+  $('.menu-close').on('click', function () {
+    $('.menu').fadeOut(200);
   });
 
-  $ ('.menu-item-mobile').on ('click', function () {
-    $ ('.menu').fadeOut (200);
+  $('.menu-item-mobile').on('click', function () {
+    $('.menu').fadeOut(200);
   });
 
   /* jquery.maskedinput */
-  $ ('.popup-form__input').mask ('+9(999) 999-9999');
-  $ ('.form-title__input').mask ('+9(999) 999-9999');
-  $ ('.offer-input').mask ('+9(999) 999-9999');
+  $('.popup-form__input').mask('+9(999) 999-9999');
+  $('.form-title__input').mask('+9(999) 999-9999');
+  $('.offer-input').mask('+9(999) 999-9999');
 
   /* Slick */
 
-  $ ('.main-slider').slick ({
+  $('.main-slider').slick({
     arrows: false,
     autoplay: true,
   });
 
-  $ ('.production-slider-top').slick ({
+  $('.production-slider-top').slick({
     arrows: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     asNavFor: '.production-slider-bottom',
-    responsive: [
-      {
-        breakpoint: 576,
-        settings: {
-          arrows: true,
-          prevArrow: '<div class="slider-arrow__mobile slider-arrow__left-mobile"></div>',
-          nextArrow: '<div class="slider-arrow__mobile slider-arrow__right-mobile"></div>',
-        },
+    responsive: [{
+      breakpoint: 576,
+      settings: {
+        arrows: true,
+        prevArrow: '<div class="slider-arrow__mobile slider-arrow__left-mobile"></div>',
+        nextArrow: '<div class="slider-arrow__mobile slider-arrow__right-mobile"></div>',
       },
-    ],
+    }, ],
   });
 
-  $ ('.production-slider-bottom').slick ({
+  $('.production-slider-bottom').slick({
     arrows: true,
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -83,14 +89,13 @@ $ (document).ready (function () {
     asNavFor: '.production-slider-top',
   });
 
-  $ ('.feedback-slider').slick ({
+  $('.feedback-slider').slick({
     arrows: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow: '<div class="slider-arrow-feedback slider-arrow-feedback__left"></div>',
     nextArrow: '<div class="slider-arrow-feedback slider-arrow-feedback__right"></div>',
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
